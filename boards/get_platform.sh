c1f8d83a24fbffdc1aa7ff90e3b29d904797f12d
swappad <luca.krueger@me.com>
2020-11-04 15:31:41 +0100

    update script for vivado 2020.1

--- boards/get_platform.sh
+++ boards/get_platform.sh
@@ -5,25 +5,29 @@ VITIS_PLATFORM=$2
 DOWNLOAD=$3
 PFM_NAME=$(echo $2 | rev | cut -d'/' -f1 | rev | cut -d'.' -f1)
 CURDIR=$(pwd)
-URL="https://www.xilinx.com/bin/public/openDownload?filename=${PFM_NAME}_2019.2.zip"
+#URL="https://www.xilinx.com/bin/public/openDownload?filename=${PFM_NAME}_2019.2.zip"
+
+# disable download; not available for 2020.1 yet
 
 # platform can be either downloaded or built from scratch
-if [[ $DOWNLOAD -eq 1 ]]; then
-	cd ${BOARD}
-	wget $URL -O ${PFM_NAME}.zip
-	unzip ${PFM_NAME}.zip
-	rm ${PFM_NAME}.zip
-else
-	if [ ! -d PYNQ-derivative-overlays ]; then
-		git clone https://github.com/yunqu/PYNQ-derivative-overlays.git
-		cd PYNQ-derivative-overlays
-		git checkout -b temp tags/v2019.2
-	fi
-	cd ${CURDIR}/PYNQ-derivative-overlays/dpu
-	make clean && make BOARD=${BOARD}
-	cd ${CURDIR}/PYNQ-derivative-overlays/vitis_platform
-	make XSA_PATH=../dpu/dpu.xsa BOARD=${BOARD}
-	cp -rf ${CURDIR}/PYNQ-derivative-overlays/vitis_platform/${BOARD}/platforms/${PFM_NAME} \
-		${CURDIR}/${BOARD}
+#if [[ $DOWNLOAD -eq 1 ]]; then
+#	cd ${BOARD}
+#	wget $URL -O ${PFM_NAME}.zip
+#	unzip ${PFM_NAME}.zip
+#	rm ${PFM_NAME}.zip
+#else
+if [ ! -d PYNQ-derivative-overlays ]; then
+	git clone https://github.com/yunqu/PYNQ-derivative-overlays.git
+	cd PYNQ-derivative-overlays
+	# checkout to commit: update scripts for 2020.1
+	git checkout ef0195dcac9169a8da04086bda0b2b4380a9097f
 fi
+cd ${CURDIR}/PYNQ-derivative-overlays/dpu
+make clean && make BOARD=${BOARD}
+cd ${CURDIR}/PYNQ-derivative-overlays/vitis_platform
+make XSA_PATH=../dpu/dpu.xsa BOARD=${BOARD}
+cp -rf ${CURDIR}/PYNQ-derivative-overlays/vitis_platform/${BOARD}/platforms/${PFM_NAME} \
+		${CURDIR}/${BOARD}
+#fi
+
 cd ${CURDIR}
