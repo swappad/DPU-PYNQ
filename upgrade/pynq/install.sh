8a03e593f0c21383ce2cc1c2cda7c21bd528dcec
swappad <luca.krueger@me.com>
2020-11-04 15:49:20 +0100

    update to pynq version 2.6.0

--- upgrade/pynq/install.sh
+++ upgrade/pynq/install.sh
@@ -5,7 +5,7 @@ set -x
 
 # check version
 cur_version=$(python3 -c "import pynq;print(pynq.__version__)")
-req_version="2.5.1"
+req_version="2.6.0"
 
 # install pynq if it is not 2.5.1
 if [[ ${cur_version} != ${req_version} ]]; then
